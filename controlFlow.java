d78d39ed46a8a430366ae124c1d28a1af38247a1
_SabJeth <91165755+cocomarktea@users.noreply.github.com>
2023-12-29 14:27:11 +0800

    Update controlFlow.java

--- controlFlow.java
+++ controlFlow.java
@@ -1,1 +1,14 @@
+class ControlFlow {
+    public static void main(String[] args) {
+        System.out.println("Odd and Even numbers"); // Display the string.
 
+        for(int i=1; i<=10; i++){
+            if(i%2==0){
+                System.out.println(i+" is even number");
+            }
+            else{
+                System.out.println(i+" is odd number");
+            }
+        };
+    }
+}
