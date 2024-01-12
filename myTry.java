83f90de3039584d8cd96b89727e553feb104db01
_SabJeth <91165755+cocomarktea@users.noreply.github.com>
2024-01-12 01:46:23 +0000

    done

--- myTry.java
+++ myTry.java
@@ -1,1 +1,15 @@
 
+class myTry{
+    public static void main(String[] args) {
+        System.out.println("naurr");
+        int count;
+        for(int i=1;i<=5;i++){
+            System.out.println(""+"\t");
+            count = 1;
+            for(int x=0;count<=5;x++){
+                count++;
+                System.out.print(" "+x);
+            }
+        }
+    }
+}
